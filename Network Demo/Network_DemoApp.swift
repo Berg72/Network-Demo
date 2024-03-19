51d28123057cfcc20fbff6416223dd3fc6f623eb
Berg72 <bergy450r@gmail.com>
2024-03-19 13:01:01 -0700

    Initial Commit

---
+++ Network Demo/Network_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_DemoApp.swift
+//  Network Demo
+//
+//  Created by Mark Bergeson on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Network_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
