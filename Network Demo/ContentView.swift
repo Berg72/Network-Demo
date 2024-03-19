f349d209abf36c619bb864472d68d1192f5f7f43
Berg72 <bergy450r@gmail.com>
2024-03-19 13:20:52 -0700

    Working with API calls

--- Network Demo/ContentView.swift
+++ Network Demo/ContentView.swift
@@ -16,6 +16,37 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear() {
+            
+            Task {
+                await apiCall()
+            }
+            
+        }
+    }
+    
+    func apiCall() async {
+        
+        
+        if let url = URL(string: "https://api.pexels.com/v1/search?query=nature&per_page=1") {
+            
+            
+            var request = URLRequest(url: url)
+            request.addValue("PWUp6sAtMGRrY1HPn8r6nOw8qOCdsc136yAdSyL6Y5m1WEDSc2cCitjh", forHTTPHeaderField: "Authorization")
+            
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request)
+                
+                print(data)
+                print(response)
+            }
+            catch {
+                print(error)
+            }
+            
+            
+        }
+        
     }
 }
 
